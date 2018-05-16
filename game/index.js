e2a8f8d38e073de51886506135f3e8f00fd9cdd4
Iggyfufu <kennethzhu07@gmail.com>
2018-05-16 12:31:31 -0400

    got node version working

--- game/index.js
+++ game/index.js
@@ -1,6 +1,22 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable'
 
-export default function reducer(state, action) {
+const MOVE = 'move'
+
+const move = (turn, position) => ({
+  type: MOVE,
+  position,
+  turn,
+})
+
+export { move }
+
+export default function reducer(state = { board: Map(), turn: 'X' }, action) {
   // TODO
+  if (action.type === MOVE) {
+    return {
+      board: state.board.setIn(action.position, action.turn),
+      turn: state.turn === 'X' ? 'O' : 'X',
+    }
+  }
   return state
 }
